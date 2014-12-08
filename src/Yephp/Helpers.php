3a3993906a69f174d28135ac164bb20080202733
Rajender Thakur <rthakur.dev@gmail.com>
2014-12-08 12:55:32 +0530

    #getIpLocation and #tzOffsetToName function added

--- src/Yephp/Helpers.php
+++ src/Yephp/Helpers.php
@@ -15,9 +15,49 @@ class Helpers{
 	   }
 	   
 	   
+	 
+	 /**
+	  * GET USER IP LOCATION 
+	  * 
+	  * @param string
+	  * 
+	  * @return Object
+	  */ 
+	  public static function getIpLocation($ip){
+			
+			try{
+				 return json_decode(file_get_contents('http://www.telize.com/geoip/'.$ip));
+				
+				}catch(Exception $e){
+				
+				return FALSE;
+			  };
+		  } 
 	   
 	   
-	   
+	/**
+	 * Timezone name
+	 * 
+	 * @param  sting  $offset
+	 * 
+	 * @return timezone name
+	 */	
+	 public static function tzOffsetToName($offset)
+		{
+				$offset *= 3600; // convert hour offset to seconds
+				$abbrarray = timezone_abbreviations_list();
+				foreach ($abbrarray as $abbr)
+						{
+							foreach ($abbr as $city)
+								{
+									if ($city['offset'] == $offset)
+									{
+										return $city['timezone_id'];
+									}
+								}
+						}
+				return FALSE;
+		}
 	   
 }
 	
